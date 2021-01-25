63ce8d74985d0facdeff384e39431555d946ded6
Ivo Dolenc <ivodolenc@gmail.com>
2021-01-25 14:45:44 +0100

    docs: add 'staggering' example

---
+++ examples/staggering/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="basic-staggering-demo">
+    <p class="el">ELEMENT</p>
+    <p class="el">ELEMENT</p>
+    <p class="el">ELEMENT</p>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.staggering()
+  },
+  methods: {
+    staggering() {
+      const anime = this.$anime
+
+      anime({
+        targets: '.basic-staggering-demo .el',
+        translateX: 270,
+        delay: anime.stagger(100) // increase delay by 100ms for each elements.
+      })
+    }
+  }
+}
+</script>
